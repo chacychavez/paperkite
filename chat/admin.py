35566bafd842a3a25e782e6687ac03f5b2220f50
chacychavez <chacychavez@gmail.com>
2022-10-08 22:14:14 +0800

    Make models modifiable in the admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Conversation, ConversationUser, Message, User
+
+admin.site.register([User, Conversation, ConversationUser, Message])
