f44b4587c704ed7b5c9b1a3919c2c4fb3b00db26
chacychavez <chacychavez@gmail.com>
2022-10-08 22:14:57 +0800

    Setup urls and views

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,30 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /chat/
     path("", views.index, name="index"),
+    # ex: chat/conversations/
+    path("conversations/", views.conversations, name="conversations"),
+    # ex: /chat/conversations/5/
+    path(
+        "conversations/<int:conversation_id>/", views.conversation, name="conversation"
+    ),
+    # ex: /chat/conversations/5/messages/
+    path(
+        "conversations/<int:conversation_id>/messages/",
+        views.messages,
+        name="messages",
+    ),
+    # ex: /chat/conversations/5/messages/5/
+    path(
+        "conversations/<int:conversation_id>/messages/<int:message_id>/",
+        views.message,
+        name="message",
+    ),
+    # ex: /chat/conversations/5/send_message/
+    path(
+        "conversations/<int:conversation_id>/send_message/",
+        views.send_message,
+        name="send_message",
+    ),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,44 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Conversation, Message
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "chat/index.html")
+
+
+def conversations(request):
+    conversations = Conversation.objects.order_by("-updated_at")
+    context = dict(conversations=conversations)
+    return render(request, "chat/conversations.html", context)
+
+
+def conversation(request, conversation_id):
+    # try:
+    #     conversation = Conversation.objects.get(pk=conversation_id)
+    # except Conversation.DoesNotExist:
+    #     raise Http404("Conversation does not exist")
+    conversation = get_object_or_404(Conversation, pk=conversation_id)
+    return render(request, "chat/conversation.html", dict(conversation=conversation))
+
+
+def messages(request, conversation_id):
+    conversation = get_object_or_404(Conversation, pk=conversation_id)
+
+    messages = Message.objects.filter(conversation_id=conversation.id).order_by(
+        "-sent_at"
+    )
+    context = dict(messages=messages)
+    return render(request, "chat/messages.html", context)
+
+
+def message(request, conversation_id, message_id):
+    get_object_or_404(Conversation, pk=conversation_id)
+    message = get_object_or_404(Message, pk=message_id)
+    return render(request, "chat/message.html", dict(message=message))
+
+
+def send_message(request, conversation_id):
+    response = f"You are sending a message to conversation {conversation_id}."
+    return HttpResponse(response)
