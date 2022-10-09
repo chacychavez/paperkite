ffb4abc2cd276fc1c5a097e400c212f111d1ad05
chacychavez <chacychavez@gmail.com>
2022-10-09 15:08:30 +0800

    Implement basic register, login, and logout

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,9 @@ app_name = "chat"
 urlpatterns = [
     # ex: /chat/
     path("", views.IndexView.as_view(), name="index"),
+    path("login", views.login_request, name="login"),
+    path("register", views.register_request, name="register"),
+    path("logout", views.logout_request, name="logout"),
     path("conversations/", views.ConversationsView.as_view(), name="conversations"),
     path(
         "conversations/<int:pk>/",
--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,60 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 
+from .forms import NewUserForm
 from .models import Conversation
 
 
 class IndexView(generic.TemplateView):
     template_name = "chat/index.html"
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("chat:index")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(
+        request=request, template_name="chat/login.html", context={"login_form": form}
+    )
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("chat:index")
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("chat:index")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(
+        request=request,
+        template_name="chat/register.html",
+        context={"register_form": form},
+    )
 
 
 class ConversationsView(generic.ListView):
